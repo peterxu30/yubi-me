dd9898acf781b596c4a8c9495b6e27887278b97b
Peter Xu <peterxu30@users.noreply.github.com>
2018-03-29 12:03:32 -0700

    simplified code

--- yubi_me.py
+++ yubi_me.py
@@ -5,20 +5,13 @@ import keyboard
 from random import randrange
 
 LETTERS = [c for c in string.ascii_lowercase]
-NUMBERS = [str(n) for n in range(10)]
-CHARS = LETTERS + NUMBERS
-CHARS_LENGTH = len(CHARS)
 
 CONFIG = {
     'min_len' : 40,
-    'max_len' : 45,
-    'has_upper' : False
+    'max_len' : 45
 }
 
 def yubi():
-    return _yubi_from_input(CHARS, CONFIG['min_len'], CONFIG['max_len'])
-
-def yubi_letters():
     return _yubi_from_input(LETTERS, CONFIG['min_len'], CONFIG['max_len'])
 
 def _yubi_from_input(src, min_length, max_length):
@@ -30,6 +23,8 @@ def _yubi_from_input(src, min_length, max_length):
         yubi_str += src[char_index]
     return yubi_str
 
-keyboard.add_hotkey('command+\\', lambda: keyboard.write(yubi_letters()))
-while True:
-    pass
+keyboard.add_hotkey('windows+\\', lambda: keyboard.write(yubi()))
+
+if __name__ == '__main__':
+    closeInput = input("Press ENTER to exit")
+    print("Closing...")
